b3399d1282b0ffb2a5c5eb137238b287c59fd1b8
Himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-09-01 22:25:37 +0530

    added some beautiful stuffs

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require ('express');
 const app = express();
+const path = require('path');
+const morgan = require('morgan')
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'))
+
+app.get('/',(req,res)=>{
 
 
+    res.contentType('text/html');
+    res.status(200);
+    res.sendFile(path.join(__dirname,'./public/html/index.html'))
+
+})
 
 
-
-app.listen(3000,()=>{
-    console.log('server work');
+app.listen(80,()=>{
+    console.log('server is running faster and better than my your life');
 });
